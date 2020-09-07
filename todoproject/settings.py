cb5a3cc032f2c82077046ad65fb598653a0039bd
github suzunaan <hiroekota114@gmail.com>
2020-09-07 20:48:48 +0900

    complete

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 try:
-    from .local_settings import *
+    from .local_setting import *
 except ImportError:
     pass
 
